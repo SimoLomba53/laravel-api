33b08f2b57c87d83299832aa7db18454df26b931
SimoLomba53 <simolomba253@gmail.com>
2023-04-13 18:46:52 +0200

    immagine presente ma non visibile

--- resources/views/admin/projs/show.blade.php
+++ resources/views/admin/projs/show.blade.php
@@ -5,6 +5,6 @@
 <section class="conteiner d-flex text-center align-items-center justify-content-center flex-column p-5">
     <strong>Title:</strong> {{ $proj->title }} <br />
     <strong>Description:</strong> {{ $proj->description }} <br />
-    <strong>Image</strong>  <img src="{{asset('storage/' . $proj->image) }} " alt=""> <br />
+    <strong>Image</strong><img src="{{asset( 'storage/' . $proj->image)}}" width="120px" height="120px" alt=""> <br />
 </section>
 @endsection
