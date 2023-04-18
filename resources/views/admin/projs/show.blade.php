f6718b1e59c9d60126a3ba69fe0083de719b46d2
SimoLomba53 <simolomba253@gmail.com>
2023-04-18 17:43:05 +0200

    type aggiunto alla show

--- resources/views/admin/projs/show.blade.php
+++ resources/views/admin/projs/show.blade.php
@@ -4,6 +4,7 @@
 
 <section class="conteiner d-flex text-center align-items-center justify-content-center flex-column p-5">
     <strong>Title:</strong> {{ $proj->title }} <br />
+    <strong>Type:</strong> {{ $proj->category?->label }} <br />
     <strong>Description:</strong> {{ $proj->description }} <br />
     <strong>Image</strong><img src="{{$proj->image}}" width="120px" height="120px" alt=""> <br />
 </section>
