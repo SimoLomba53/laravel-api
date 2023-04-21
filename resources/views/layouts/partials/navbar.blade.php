1fd96baa25edb2edbb35b21e4971f9091dc6860d
SimoLomba53 <simolomba253@gmail.com>
2023-04-21 15:49:14 +0200

    MILESTONE 2 verifica Postman

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -17,7 +17,7 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
+                     <!-- Left Side Of Navbar -->
                     @auth
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
